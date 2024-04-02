facab7b3f0ca31e161387ae6d7046c6c388a9624
Aniket Sharma <aniket.sharma.4509@gmail.com>
2024-04-02 23:26:10 +0530

    Updated Start/Stop Scripts

--- docker/stop.sh
+++ docker/stop.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-source docker.creds
-
-echo ${USERNAME}
-docker login -u "${USERNAME}" -p "${PASSWORD}"
-
-docker-compose pull cloudkeeper
-
-docker-compose up -d
+docker-compose down
