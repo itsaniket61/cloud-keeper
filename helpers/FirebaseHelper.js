1c2e409b6bedbd9276903406094879092a5256dc
Aniket Sharma <aniket.sharma.4509@gmail.com>
2024-04-04 08:10:11 +0530

    Updated Code to get Firebase Config from Path

--- helpers/FirebaseHelper.js
+++ helpers/FirebaseHelper.js
@@ -1,18 +1,31 @@
 import * as admin from 'firebase-admin';
+import fs from 'fs';
 
-const serviceAccount = {
-  type: 'service_account',
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: 'googleapis.com',
-};
+let serviceAccount;
+if(process.env.FIREBASE_CONFIG_PATH) {
+  const path = process.env.FIREBASE_CONFIG_PATH;
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    firebaseConfig = JSON.parse(data);
+  } catch (err) {
+    console.error('Error reading Firebase config file:', err);
+    throw new Error('Error reading Firebase config file:', err);
+  }
+}else {
+  serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: 'googleapis.com',
+  };
+}
 
 if (!admin.apps.length && serviceAccount.project_id) {
   admin.initializeApp({
